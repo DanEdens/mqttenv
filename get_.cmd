abd299832239e925a1625a1a700d2dff5be22621
John Edens <dan.edens@geo-instruments.com>
2021-08-13 08:54:13 -0500

    updates sripts

--- get_.cmd
+++ get_.cmd
@@ -1,7 +1,8 @@
+@echo off
 set "name=%1"
 for %%a in ("%name: ="=:="%") do set "last=%%~a"
 FOR /F "tokens=* delims=" %%g IN ('mosquitto_sub -h %awsip% -p %awsport% -t %1 -C 1') do (SET get=%%g)
-@REM set name=%name:/=_%
+set name=%name:/=_%
 set last=%get%
 echo %name% = %last%
 set %name%=%last%
