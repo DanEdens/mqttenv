108f4f5dabe7a9ccaa707fc48c8482f063966698
John Edens <dan.edens@geo-instruments.com>
2021-10-29 13:29:14 -0500

    update

--- eget.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-@set "name=%1"
-@set "output=%2"
-@for %%a in ("%name: ="=:="%") do @set "last=%%~a"
-@FOR /F "tokens=* delims=" %%g IN ('@mosquitto_sub -h %awsip% -p %awsport% -t %1 -C 1') do (SET get=%%g)
-@set last=%get%
-@echo %output% = %last%
-@set %output%=%last%
--- rost.cmd
+++ rost.cmd
@@ -1,4 +1,5 @@
+@echo off
 set _tail=%*
-@call set _tail=%%_tail:*%1 =%%
-@echo Trying: %awsip%:%awsport%
-mosquitto_pub -r -h %awsip% -p %awsport% -i %DENA% -t "%1" -m "%_tail%" && @echo Topic: %1 && @echo Msg: %_tail%
+call set _tail=%%_tail:*%1 =%%
+echo Trying: %awsip%:%awsport%
+mosquitto_pub -r -h %awsip% -p %awsport% -i %DENA% -t "%1" -m "%_tail%" && echo Topic: %1 && echo Msg: %_tail%
